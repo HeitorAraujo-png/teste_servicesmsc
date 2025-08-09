79a398cfdd52ace9f1ec7f43f90473ebce37c476
Heitor Araujo <heitoraraujodev@gmail.com>
2025-08-09 15:44:09 -0400

    formatação

--- codigo/services.py
+++ codigo/services.py
@@ -15,12 +15,12 @@ class Relatorio:
         self.departamento = pd.read_csv(self.LinkPathPTD, encoding="latin1")
         self.nomes, self.centros = self.CentroCustos()
         self.Adicionar()
-    
+
     def Concatena(self, arquivos):
         """Concatena os dois arquivos .XLSX tranformando em um unico arquivo .CSV"""
         self.departamento = pd.DataFrame()
         for i in arquivos:
-            i = fr'{BASE_DIR}\{RELATORIO}\{i}'
+            i = rf"{BASE_DIR}\{RELATORIO}\{i}"
             relatorio = pd.read_excel(i, parse_dates=["DATA"])
             relatorio["DATA"] = relatorio["DATA"].dt.strftime("%d/%m/%y")
             self.departamento = pd.concat([self.departamento, relatorio], ignore_index=True)
@@ -29,11 +29,11 @@ class Relatorio:
     def Adicionar(self):
         """Adiciona os dias no arquivo .CSV que contem as principais informações"""
 
-        colunas = ['DPT'] + self.dias + ['Total', 'Valor total', '%']
+        colunas = ["DPT"] + self.dias + ["Total", "Valor total", "%"]
         lista = {col: None for col in colunas}
         self.relatorio = pd.DataFrame([lista])
         self.relatorio = self.relatorio.drop(0)
-        self.relatorio.to_csv(fr'{BASE_DIR}\{UPLOADCSV}\relatorio.csv')
+        self.relatorio.to_csv(rf"{BASE_DIR}\{UPLOADCSV}\relatorio.csv")
 
     def remove(self, lista):
         """Remove itens duplicados de uma lista"""
@@ -45,11 +45,11 @@ class Relatorio:
 
     def TakeIndex(self, search):
         """Pega index de coluna especifica\n
-    Exemplo: .to_list()) vai voltar uma lista com todas as colunas do arquivo
-    ['dpt', '28/11/2024', '29/11/2024', '30/11/2024', 'Total', 'Valor total', '%']
-    Vamos dizer que search = 'Valor total'
-    index = self.relatorio.columns.to_list().index(search vai retornar 5
-    return result == 'F'
+        Exemplo: .to_list()) vai voltar uma lista com todas as colunas do arquivo
+        ['dpt', '28/11/2024', '29/11/2024', '30/11/2024', 'Total', 'Valor total', '%']
+        Vamos dizer que search = 'Valor total'
+        index = self.relatorio.columns.to_list().index(search vai retornar 5
+        return result == 'F'
         """
         index = self.relatorio.columns.to_list().index(search)
         result = ""
@@ -65,8 +65,8 @@ class Relatorio:
         lista = []
         for i in centro:
             i = str(i)
-            ic = f'{i[0]}{i[1]}'
-            cc = f'{i[2]}{i[3]}'
+            ic = f"{i[0]}{i[1]}"
+            cc = f"{i[2]}{i[3]}"
             if ic == "81":
                 emp = "TVCA"
             if ic == "65":
@@ -101,22 +101,11 @@ class Relatorio:
         for dia in self.dias:
             total_dia = 0
             for cc in self.centros:
-                qtd = len(
-                    self.departamento[
-                        (self.departamento["C.C"] == cc)
-                        & (self.departamento["DATA"] == dia)
-                    ]
-                )
+                qtd = len(self.departamento[(self.departamento["C.C"] == cc) & (self.departamento["DATA"] == dia)])
                 total_dia += qtd
-            atual[dia] = (
-                f"=SUM({self.TakeIndex(dia)}{coluna}:{self.TakeIndex(dia)}{len(self.centros) + 1})"
-            )
-        atual["Valor total"] = (
-            f"=SUM({self.TakeIndex('Valor total')}{coluna}:{self.TakeIndex('Valor total')}{len(self.centros) + 1})"
-        )
-        atual["Total"] = (
-            f"=SUM({self.TakeIndex('Total')}{coluna}:{self.TakeIndex('Total')}{len(self.centros) + 1})"
-        )
+            atual[dia] = (f"=SUM({self.TakeIndex(dia)}{coluna}:{self.TakeIndex(dia)}{len(self.centros) + 1})")
+        atual["Valor total"] = (f"=SUM({self.TakeIndex('Valor total')}{coluna}:{self.TakeIndex('Valor total')}{len(self.centros) + 1})")
+        atual["Total"] = (f"=SUM({self.TakeIndex('Total')}{coluna}:{self.TakeIndex('Total')}{len(self.centros) + 1})")
         for cc, nome in zip(self.centros, self.nomes):
             nova_linha = {"DPT": nome}
             soma = 0
@@ -124,30 +113,17 @@ class Relatorio:
                 qtd = len(
                     self.departamento[
                         (self.departamento["C.C"] == cc)
-                        & (self.departamento["DATA"] == dia)
-                    ]
-                )
+                        & (self.departamento["DATA"] == dia)])
                 nova_linha[dia] = qtd
                 soma += qtd
-            nova_linha["%"] = (
-                f"=SUM({self.TakeIndex('Valor total')}{coluna}/{self.TakeIndex('Valor total')}{len(self.centros) + 2})"
-            )
-            nova_linha["Total"] = (
-                f"=SUM({self.TakeIndex(self.dias[0])}{coluna}:{self.TakeIndex(self.dias[-1])}{coluna})"
-            )
+            nova_linha["%"] = (f"=SUM({self.TakeIndex('Valor total')}{coluna}/{self.TakeIndex('Valor total')}{len(self.centros) + 2})")
+            nova_linha["Total"] = (f"=SUM({self.TakeIndex(self.dias[0])}{coluna}:{self.TakeIndex(self.dias[-1])}{coluna})")
             nova_linha["Valor total"] = f"=${self.TakeIndex('Total')}${coluna} * 20"
-            self.relatorio = pd.concat(
-                [self.relatorio, pd.DataFrame([nova_linha])], ignore_index=True
-            )
+            self.relatorio = pd.concat([self.relatorio, pd.DataFrame([nova_linha])], ignore_index=True)
             coluna = coluna + 1
-        atual["%"] = (
-            f"=SUM({self.TakeIndex('%')}{2}:{self.TakeIndex('%')}{len(self.centros) + 1})"
-        )
-        self.relatorio = pd.concat(
-            [self.relatorio, pd.DataFrame([atual])], ignore_index=True
-        )
-        self.relatorio.to_excel(
-                rf"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx", index=False)
+        atual["%"] = (f"=SUM({self.TakeIndex('%')}{2}:{self.TakeIndex('%')}{len(self.centros) + 1})")
+        self.relatorio = pd.concat([self.relatorio, pd.DataFrame([atual])], ignore_index=True)
+        self.relatorio.to_excel(rf"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx", index=False)
 
     def Espaco(self):
         """Arruma o espaçamento de cada coluna do arquivo em excel"""
@@ -167,12 +143,11 @@ class Relatorio:
             ajuste = max_l + 2
             ws.column_dimensions[coluna_letra].width = ajuste
             self.Salva()
-        
+
     def Salva(self):
         hoje = datetime.now().strftime("%d-%m-%y-")
-        if os.path.exists((fr"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx")):
-            self.wb.save(fr"{BASE_DIR}\{HISTORICO}\{hoje}relatorio_atualizado.xlsx")
-            self.relatorio.to_excel(
-                rf"{BASE_DIR}\{HISTORICO}\{hoje}relatorio_atualizado.xlsx", index=False)
+        if os.path.exists((rf"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx")):
+            self.wb.save(rf"{BASE_DIR}\{HISTORICO}\{hoje}relatorio_atualizado.xlsx")
+            self.relatorio.to_excel(rf"{BASE_DIR}\{HISTORICO}\{hoje}relatorio_atualizado.xlsx", index=False)
         else:
-            self.wb.save(fr"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx")
+            self.wb.save(rf"{BASE_DIR}\{UPLOADXLSX}\relatorio_atualizado.xlsx")
